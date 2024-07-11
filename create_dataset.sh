5e90084105bc5d6b4a51212ec2bfd4908fbd7ce8
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-07-11 11:56:46 +0000

    deleted unused files and renamed the ones with new dataset

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --no-requeue
-#SBATCH -J create_dataset
+#SBATCH -J create_hf_dataset
 #SBATCH --get-user-env
 #SBATCH --partition=THIN
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH -o create_dataset.out
+#SBATCH -o create_hf_dataset.out
 #SBATCH --mem=0
 #SBATCH --time=01:00:00
 
 source environment/bin/activate
 
-python -u source/create_dataset.py
+python -u source/create_dataset_hf.py
 echo "done"
